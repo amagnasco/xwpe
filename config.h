0d823b174ac721e33557d506479126fceca80907
Guus Bonnema <gbonnema@xs4all.nl>
2017-07-08 15:04:10 +0200

    executed autoreconf anew

--- config.h
+++ config.h
@@ -102,6 +102,9 @@
 /* Define to 1 if you have the `strstr' function. */
 #define HAVE_STRSTR 1
 
+/* Define to 1 if you have the `symlink' function. */
+#define HAVE_SYMLINK 1
+
 /* Define to 1 if you have the <sys/ioctl.h> header file. */
 #define HAVE_SYS_IOCTL_H 1
 
@@ -113,6 +116,9 @@
 
 /* Define to 1 if you have the <termios.h> header file. */
 #define HAVE_TERMIOS_H 1
+
+/* Define to 1 if you have the `tparm' function. */
+#define HAVE_TPARM 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
