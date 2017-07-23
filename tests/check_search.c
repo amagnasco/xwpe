d05a0afcf4406985ea4b828b44b0e1bb78967e52
Guus Bonnema <gbonnema@xs4all.nl>
2017-07-23 15:16:30 +0200

    Preparing autotools for tests with check (part 1). make check does not work yet. * Modified .deps and .libs in gitignore to be ignore in every subdirectory. * Added tests subdirectory plus Makefile.am * Added check for library check in configure.ac * Created first header and c-file for tests (dummy for now)

---
+++ tests/check_search.c
@@ -0,0 +1,28 @@
+/**
+ *  XWPE - XWindows Programming Editor
+ *  Copyright (C) 2017 Guus Bonnema.
+    This file is part of Xwpe - Xwindows Programming Editor.
+
+    Xwpe - Xwindows Programming Editor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Xwpe - Xwindows Programming Editor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Xwpe - Xwindows Programming Editor.  If not, see <http://www.gnu.org/licenses/>.
+
+   */
+#include <check.h>
+#include "check_search.h"
+
+int main(void)
+{
+	return(0);
+}
+
+
---
+++ tests/check_search.h
@@ -0,0 +1,22 @@
+/**
+    This file is part of Xwpe - Xwindows Programming Editor.
+
+    Xwpe - Xwindows Programming Editor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Xwpe - Xwindows Programming Editor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Xwpe - Xwindows Programming Editor.  If not, see <http://www.gnu.org/licenses/>.
+
+   */
+#ifndef CHECK_SEARCH_H
+#define CHECK_SEARCH_H
+
+#endif
+
