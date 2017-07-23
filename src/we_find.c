a521d3e7d7f9a41399906222bc49c4fd2c36c579
Guus Bonnema <gbonnema@xs4all.nl>
2017-07-23 10:21:21 +0200

    Modified find algorithm and struct (not integrated yet), still using old. Also, switched from using indent for formatting to astyle, which works better. Default style for astyle is "--style=allman".

--- src/we_find.c
+++ src/we_find.c
@@ -20,75 +20,155 @@
 #include "we_find.h"
 #include "utils.h"
 
+/* non exported prototypes */
+/**
+ * Allocates memory for the specified number of Match occurrences.
+ * if the pointer ptr is NULL, new memory is allocated, otherwise
+ * the memory is reallocated. If the pointer ptr is non-NULL and size
+ * is zero, the effect is of a free.
+ *
+ *
+ *
+ * @param ptr pointer to Match array. may be NULL
+ * @param size size_t number of occurrences of Match, may be zero
+ * @param err_msg string to be printed on error, may be NULL.
+ *
+ * @return pointer to Match. If reallocation is successful, this pointer
+ * is the same as the param ptr. If reallocation is not successful, this
+ * pointer is NULL and ptr is unchanged.
+ *
+ */
+Match *allocate_match (Match * ptr, size_t size, const char *err_msg);
+/**
+ * Initialize a new Search_result.
+ *
+ */
+Search_result new_search_result ();
+
+Search_result
+e_search_line (Search_request * request)
+{
+    size_t size_result = 10;
+    char *alloc_err_msg;
+    sprintf (alloc_err_msg, "Request to look for '%s' failed due to memory problems.\n",
+             request->needle);
+
+    Search_result result = new_search_result ();
+
+    if (!allocate_match (result.matches, size_result, alloc_err_msg))
+    {
+        result.match_result = error;
+        return result;
+    }
+    result.nr_hits = 0;
+    result.match_result = match_not_found;
+
+    const char *haystack = (const char *) request->haystack;
+    const char *needle = (const char *) request->needle;
+    char *match = strstr (haystack, needle);
+    while (haystack)
+    {
+        if (!match)
+            break;
+        result.nr_hits++;
+        result.match_result = match_found;
+        if (result.nr_hits > size_result)
+        {
+            size_result *= 2;	/* double the size */
+            void *ptr = allocate_match (result.matches, size_result, alloc_err_msg);
+            if (!ptr)
+            {
+                result.match_result = error;
+                return result;
+            }
+        }
+        match = strstr (haystack, needle);
+        haystack++;
+    }
+    return result;
+}
+
+Search_result
+new_search_result ()
+{
+    Search_result result;
+    result.match_result = 0;
+    strcpy (result.error_msg, "");
+    result.nr_hits = 0;
+    result.matches = NULL;
+    return result;
+}
+
+Match *
+allocate_match (Match * ptr, size_t size, const char *err_msg)
+{
+    void *return_ptr = realloc (ptr, size * sizeof (Match));
+    if (!return_ptr)
+    {
+        printf ("Reallocation of search memory failed.\n");
+        printf ("size of the request was %ld.\n", size);
+        if (err_msg)
+            printf ("provided error msg: %s\n", err_msg);
+        printf
+        ("This should never happen unless memory is really at a minimum.\n");
+        printf ("Breaking off the search algorithm.");
+        return NULL;
+    }
+    return return_ptr;
+}
 
 /*        find string in text line    */
 int
 e_strstr (int start_offset, int end_offset, unsigned char *search_string,
-	  unsigned char *search_expression)
+          unsigned char *search_expression)
 {
-  int i, j, len_search_exp = strlen ((const char *) search_expression);
+    int len_search_exp = strlen ((const char *) search_expression);
+    int (*compare) (const char *s1, const char *s2, size_t n);
+    compare = strncmp;
 
-  if (start_offset > end_offset)
+    if (start_offset > end_offset)
     {
-      for (i = start_offset - len_search_exp; i >= end_offset; i--)
-	{
-	  for (j = 0; j < len_search_exp; j++)
-	    if (search_string[i + j] != search_expression[j])
-	      break;
-	  if (j == len_search_exp)
-	    return (i);
-	}
+        for (int i = start_offset - len_search_exp; i >= end_offset; i--)
+            if (0 == compare ((const char *) search_string + i,
+                              (const char *) search_expression, len_search_exp))
+                return (i);
     }
-  else
+    else
     {
-      for (i = start_offset >= 0 ? start_offset : 0;
-	   i <= end_offset - len_search_exp
-	   /* && search_string[i] != '\0' && search_string[i] != WR */ ;
-	   i++)
-	{
-	  for (j = 0; j < len_search_exp; j++)
-	    if (search_string[i + j] != search_expression[j])
-	      break;
-	  if (j == len_search_exp)
-	    return (i);
-	}
+        for (int i = start_offset >= 0 ? start_offset : 0;
+                i <= end_offset - len_search_exp; i++)
+            if (0 == compare ((const char *) search_string + i,
+                              (const char *) search_expression, len_search_exp))
+                return (i);
     }
-  return (-1);
+    return (-1);
 }
 
 /*        Find string in line (ignoring case)   */
 int
 e_ustrstr (int start_offset, int end_offset, unsigned char *search_string,
-	   unsigned char *search_expression)
+           unsigned char *search_expression)
 {
-  int i, j, len_search_exp = strlen ((const char *) search_expression);
+    int len_search_exp = strlen ((const char *) search_expression);
+    int (*compare) (const char *s1, const char *s2, size_t n);
+    compare = strncasecmp;
 
-  if (start_offset > end_offset)
+    if (start_offset > end_offset)
     {
-      for (i = start_offset - len_search_exp; i >= end_offset; i--)
-	{
-	  for (j = 0; j < len_search_exp; j++)
-	    if (toupper (search_string[i + j]) !=
-		toupper (search_expression[j]))
-	      break;
-	  if (j == len_search_exp)
-	    return (i);
-	}
+        for (int i = start_offset - len_search_exp; i >= end_offset; i--)
+            if (0 == compare ((const char *) search_string + i,
+                              (const char *) search_expression, len_search_exp))
+                return (i);
     }
-  else
+    else
     {
-      for (i = start_offset < 0 ? 0 : start_offset;
-	   i <= end_offset - len_search_exp; i++)
-	{
-	  for (j = 0; j < len_search_exp; j++)
-	    if (toupper (search_string[i + j]) !=
-		toupper (search_expression[j]))
-	      break;
-	  if (j == len_search_exp)
-	    return (i);
-	}
+        for (int i = start_offset < 0 ? 0 : start_offset;
+                i <= end_offset - len_search_exp; i++)
+            if (0 == compare ((const char *) search_string + i,
+                              (const char *) search_expression, len_search_exp))
+                return (i);
     }
-  return (-1);
+    return (-1);
 }
 
 /* function e_rstrstr
@@ -108,146 +188,147 @@ e_ustrstr (int start_offset, int end_offset, unsigned char *search_string,
  *   */
 int
 e_rstrstr (size_t start_offset,
-	   size_t end_offset,
-	   unsigned char *search_string,
-	   unsigned char *regular_expression,
-	   size_t * end_match, _Bool case_sensitive)
+           size_t end_offset,
+           unsigned char *search_string,
+           unsigned char *regular_expression,
+           size_t * end_match, _Bool case_sensitive)
 {
-  regex_t regz[1];
-  regmatch_t *matches = NULL;
-  size_t start, end, nr_chars_search_str;
-  size_t nr_matches;
-  int start_match;
+    regex_t regz[1];
+    regmatch_t *matches = NULL;
+    size_t start, end, nr_chars_search_str;
+    size_t nr_matches;
+    int start_match;
 
-  // copy and sanity check the input variables
-  nr_chars_search_str = strlen ((const char *) search_string);
-  start = start_offset;
-  end = end_offset;
-  if (start > end || start > nr_chars_search_str || end > nr_chars_search_str)
+    // copy and sanity check the input variables
+    nr_chars_search_str = strlen ((const char *) search_string);
+    start = start_offset;
+    end = end_offset;
+    if (start > end || start > nr_chars_search_str || end > nr_chars_search_str)
     {
-      char *msg =
-	"Regex search with: start=%lu, end=%lu, string=\"%s\", reg_exp=\"%s\"\n";
-      printf (msg, start, end, search_string, regular_expression);
-      print_stacktrace ();
-      return -1;
+        char *msg =
+            "Regex search with: start=%lu, end=%lu, string=\"%s\", reg_exp=\"%s\"\n";
+        printf (msg, start, end, search_string, regular_expression);
+        print_stacktrace ();
+        return -1;
     }
-  unsigned char str[nr_chars_search_str + 1];
-  /* copy excluding null byte */
-  strncpy ((char *) str, (const char *) search_string, nr_chars_search_str);
-  str[nr_chars_search_str] = '\0';
+    unsigned char str[nr_chars_search_str + 1];
+    /* copy excluding null byte */
+    strncpy ((char *) str, (const char *) search_string, nr_chars_search_str);
+    str[nr_chars_search_str] = '\0';
 
-  // Compile regular expression, return -1 if compile fails.
-  int cflags = case_sensitive
-    ? REG_EXTENDED | REG_NEWLINE : REG_EXTENDED | REG_NEWLINE | REG_ICASE;
-  if (regcomp (regz, (const char *) regular_expression, cflags))
+    // Compile regular expression, return -1 if compile fails.
+    int cflags = case_sensitive
+                 ? REG_EXTENDED | REG_NEWLINE : REG_EXTENDED | REG_NEWLINE | REG_ICASE;
+    if (regcomp (regz, (const char *) regular_expression, cflags))
     {
-      return -1;
+        return -1;
     }
-  // Note the number of submatches the compile has found and add 1 for the entire match.
-  // Use only the entire match. 
-  nr_matches = regz->re_nsub + 1;
-  matches = malloc (nr_matches * sizeof (regmatch_t));
-  unsigned char old = search_string[end];	/* Save char */
-  str[end] = '\0';
-  int search_result =
-    regexec (regz, (const char *) &search_string[start], nr_matches, matches,
-	     REG_NOTBOL | REG_NOTEOL);
-  str[end] = old;		/* Restore char */
-  if (search_result == REG_NOMATCH)
+    // Note the number of submatches the compile has found and add 1 for the entire match.
+    // Use only the entire match.
+    nr_matches = regz->re_nsub + 1;
+    matches = malloc (nr_matches * sizeof (regmatch_t));
+    unsigned char old = search_string[end];	/* Save char */
+    str[end] = '\0';
+    int search_result =
+        regexec (regz, (const char *) &search_string[start], nr_matches,
+                 matches,
+                 REG_NOTBOL | REG_NOTEOL);
+    str[end] = old;		/* Restore char */
+    if (search_result == REG_NOMATCH)
     {
-      regfree (regz);		/* Obliged internal free */
-      free (matches);
-      return -1;
+        regfree (regz);		/* Obliged internal free */
+        free (matches);
+        return -1;
     }
-  if (search_result != 0)
+    if (search_result != 0)
     {
-      char err_buff[1024];
-      regerror (search_result, regz, err_buff, 1024);
-      printf ("[regex search] error message: %s\n", err_buff);
-      regfree (regz);		/* Obliged internal free */
-      free (matches);
-      return -1;		/* Error other than not found */
+        char err_buff[1024];
+        regerror (search_result, regz, err_buff, 1024);
+        printf ("[regex search] error message: %s\n", err_buff);
+        regfree (regz);		/* Obliged internal free */
+        free (matches);
+        return -1;		/* Error other than not found */
     }
-  start_match =
-    (matches[0].rm_so <
-     (signed long) start) ? matches[0].rm_so : (signed long) start;
-  *end_match =
-    (matches[0].rm_eo >
-     (signed long) end) ? matches[0].rm_eo : (signed long) end;
+    start_match =
+        (matches[0].rm_so <
+         (signed long) start) ? matches[0].rm_so : (signed long) start;
+    *end_match =
+        (matches[0].rm_eo >
+         (signed long) end) ? matches[0].rm_eo : (signed long) end;
 
-  regfree (regz);		/* Obliged internal free */
-  free (matches);
-  return start_match;
+    regfree (regz);		/* Obliged internal free */
+    free (matches);
+    return start_match;
 }
 
 _Bool
 find_successful (unsigned int sw)
 {
-  return (sw & 1) != 0;
+    return (sw & 1) != 0;
 }
 
 _Bool
 find_from_cursor (unsigned int sw)
 {
-  return (sw & 2) == 0;
+    return (sw & 2) == 0;
 }
 
 _Bool
 find_entire_scope (unsigned int sw)
 {
-  return (sw & 2) != 0;
+    return (sw & 2) != 0;
 }
 
 _Bool
 find_search_forward (unsigned int sw)
 {
-  return (sw & 4) == 0;
+    return (sw & 4) == 0;
 }
 
 _Bool
 find_search_backward (unsigned int sw)
 {
-  return (sw & 4) != 0;
+    return (sw & 4) != 0;
 }
 
 _Bool
 find_global_scope (unsigned int sw)
 {
-  return (sw & 8) == 0;
+    return (sw & 8) == 0;
 }
 
 _Bool
 find_selection (unsigned int sw)
 {
-  return (sw & 8) != 0;
+    return (sw & 8) != 0;
 }
 
 _Bool
 find_confirm_replace (unsigned int sw)
 {
-  return (sw & 16) != 0;
+    return (sw & 16) != 0;
 }
 
 _Bool
 find_regular_expression (unsigned int sw)
 {
-  return (sw & 32) != 0;
+    return (sw & 32) != 0;
 }
 
 _Bool
 find_word_boundary (unsigned int sw)
 {
-  return (sw & 64) != 0;
+    return (sw & 64) != 0;
 }
 
 _Bool
 find_ignore_case (unsigned int sw)
 {
-  return (sw & 128) == 0;
+    return (sw & 128) == 0;
 }
 
 _Bool
 find_case_sensitive (unsigned int sw)
 {
-  return (sw & 128) != 0;
+    return (sw & 128) != 0;
 }
--- src/we_find.h
+++ src/we_find.h
@@ -1,5 +1,5 @@
-#ifndef WE_FINE_H
-#define WE_FINE_H
+#ifndef WE_FIND_H
+#define WE_FIND_H
 /**
  *  we_find.h
  *
@@ -14,59 +14,98 @@
  *
  */
 
+#include <string.h>
 #include "config.h"
+
+typedef enum
+{ forward_search, backward_search } Search_direction;
 
 /* Struct definitions */
 
 /**
- * The search functions use this struct to store the 
- * results of the search
+ * Use this struct in a search request.
  *
  */
 typedef struct
 {
-  int nr_hits;			/* -1 if failed, 0 if failed, 1 if success */
-  size_t start_match;		/* offset of matched substring within string */
-  size_t end_match;		/* offset of last matched char within string */
+    unsigned char *haystack;	/* the string to be searched */
+    unsigned char *needle;	/* the string we are searching for */
+    Search_direction search_direction;
+    size_t start_offset;		/* offset within the string to start search */
+    size_t end_offset;		/* offset with the string to end search */
+    /* use a function like strcmp, strncmp, strcasecmp, strncasecmp for compare
+     * You can also write your own as long as it has the same signature and comparable behaviour.
+     */
+    int (*compare) (const char *s1, const char *s2, size_t n);
+} Search_request;
+
+typedef struct
+{
+    size_t start_match;		/* offset of matched substring within string */
+    size_t end_match;		/* offset of last matched char within string */
+} Match;
+
+#define WE_MAX_ERR 1024
+
+/**
+ * The search functions use this struct to store the results of the search
+ */
+typedef struct
+{
+    enum {error, match_found, match_not_found} match_result;
+    char error_msg[WE_MAX_ERR];	/* on error, the error_message contains a string */
+    size_t nr_hits;				/* The number of matches, and the length of the matches array */
+    size_t nr_matches_allocated;	/* The size of the matches array */
+    Match *matches;				/* The set of matches for the search string provided */
 } Search_result;
 
 /**
- * The FIND structure is in use for searching. 
- *
+ * Use this struct in a search request by regular expression.
+ */
+typedef struct
+{
+    Search_direction search_direction;
+    size_t start_offset;		/* offset within the string to start search */
+    size_t end_offset;		/* offset with the string to end search */
+    _Bool case_sensitive;		/* true for case sensitive, false to ignore case */
+} Regex_request;
+
+/**
+ * The FIND structure is in use for searching.
  */
 typedef struct FND
 {
-  char search[80], replace[80];
-  char file[80];		/* filename or pattern to search/open */
-  char *dirct;
-  size_t sn;			/* length of the search string */
-  size_t rn;			/* length of the replace string */
-  /** sw is a binary field where each bit has a logical value
-   *
-   * bit  | test     | zero              | one                |
-   * val  |          | off               | on                 |
-   * ------------------------------------------------------  
-   *  2^0 | sw & 1   |                   | successful         |
-   *  2^1 | sw & 2   | from cursor       | entire scope       |
-   *  2^2 | sw & 4   | search forward    | search backward    |
-   *  2^3 | sw & 8   | global scope      | selection          |
-   *  2^4 | sw & 16  |                   | confirm replace    |
-   *  2^5 | sw & 32  |                   | regular expression |
-   *  2^6 | sw & 64  |                   | word boundary      |
-   *  2^7 | sw & 128 | ignore case       | case sensitive     |
-   *  2^8 | sw & 256 | 
-   *  2^9 | sw & 512 | 
-   *  2^10| sw & 1024| 
-   *
-   * The name of the corresponding functions that test correctly
-   * you can find by prefixing the description with "find_" and
-   * replace space by "_". bit 3 (sw & 8) you can test with 
-   * find_global_scope(find->sw). The other values are reserved for now.
-   *
-   * You can find these values defined in we_block.c: e_find and e_replace
-   *
-   */
-  unsigned int sw;
+    char search[80], replace[80];
+    char file[80];		/* filename or pattern to search/open */
+    char *dirct;
+    size_t sn;			/* length of the search string */
+    size_t rn;			/* length of the replace string */
+    /** sw is a binary field where each bit has a logical value
+     *
+     * bit  | test     | zero              | one                |
+     * val  |          | off               | on                 |
+     * ------------------------------------------------------
+     *  2^0 | sw & 1   |                   | successful         |
+     *  2^1 | sw & 2   | from cursor       | entire scope       |
+     *  2^2 | sw & 4   | search forward    | search backward    |
+     *  2^3 | sw & 8   | global scope      | selection          |
+     *  2^4 | sw & 16  |                   | confirm replace    |
+     *  2^5 | sw & 32  |                   | regular expression |
+     *  2^6 | sw & 64  |                   | word boundary      |
+     *  2^7 | sw & 128 | ignore case       | case sensitive     |
+     *  2^8 | sw & 256 |
+     *  2^9 | sw & 512 |
+     *  2^10| sw & 1024|
+     *
+     * The name of the corresponding functions that test correctly
+     * you can find by prefixing the description with "find_" and
+     * replace space by "_". bit 3 (sw & 8) you can test with
+     * find_global_scope(find->sw). The other values are reserved for now.
+     *
+     * You can find these values defined in we_block.c: e_find and e_replace
+     *
+     */
+    unsigned int sw;
 } FIND;
 
 
@@ -85,11 +124,20 @@ _Bool find_word_boundary (unsigned int sw);
 _Bool find_ignore_case (unsigned int sw);
 _Bool find_case_sensitive (unsigned int sw);
 
+/**
+ *  Searches for a fixed string in a text depending on the
+ *  provided search request criteria (includes the search string).
+ *
+ *  Search expression must have non-zero length.
+ *
+ */
+Search_result e_search_line (Search_request * request);
+
 int e_strstr (int x, int n, unsigned char *s, unsigned char *f);
 int e_ustrstr (int x, int n, unsigned char *s, unsigned char *f);
 int e_rstrstr (size_t start_offset, size_t end_offset,
-	       unsigned char *search_string,
-	       unsigned char *regular_expression,
-	       size_t * end_match_str, _Bool case_sensitive);
+               unsigned char *search_string,
+               unsigned char *regular_expression,
+               size_t * end_match_str, _Bool case_sensitive);
 
 #endif
