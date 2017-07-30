3bcb3e22f6a49e36c6533644b7481fac50ebfe7c
Guus Bonnema <gbonnema@xs4all.nl>
2017-07-30 04:59:45 +0200

    Moved tests subdirectory to test subdirectory.

--- test/check_search.c
+++ test/check_search.c
@@ -22,8 +22,21 @@
 #include "../src/we_find.h"
 #include "check_search.h"
 
-START_TEST(test_search_null_request)
+START_TEST(test_search_regex_string)
 {
+	unsigned char *str = (unsigned char *)"This is a long string.\n";
+	int start_offset = 0;
+	int end_offset = strlen((const char *)str);
+	unsigned char *reg_expr = (unsigned char *)"\bis\b";
+	_Bool case_sensitive = 1;
+	size_t end_match = 0;
+	int start_match = e_rstrstr(start_offset, end_offset, 
+			str, reg_expr, &end_match, case_sensitive);
+	int expected_start_match = 5;
+	size_t expected_end_match = 6;
+	ck_assert_int_eq(expected_start_match,start_match);
+	ck_assert_int_eq(expected_end_match, end_match);
+
 }
 END_TEST
 
@@ -46,7 +59,7 @@ Suite * search_suite(void)
 
 	tc_search = tcase_create( "Search Test" );
 
-//	tcase_add_test(tc_search, test_search_null_request);
+	tcase_add_test(tc_search, test_search_regex_string);
 //	tcase_add_test(tc_search, test_search_null);
 //	tcase_add_test(tc_search, test_search_normal);
 	suite_add_tcase(s, tc_search);
